3db5a31ba223e3515de49d5994146cca0ffe8564
vivisthebest <watermelon.star@gmail.com>
2015-11-24 21:06:18 -0500

    test if can get restaurant parameter

--- restaurant.php
+++ restaurant.php
@@ -11,5 +11,23 @@
     </head>
     <body>
         <?php include_once('header.php'); ?>
+        
+        <script type="text/javascript">
+            var getUrlParameter = function getUrlParameter(sParam) {
+                var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+                    sURLVariables = sPageURL.split('&'),
+                    sParameterName,
+                    i;
+            
+                for (i = 0; i < sURLVariables.length; i++) {
+                    sParameterName = sURLVariables[i].split('=');
+            
+                    if (sParameterName[0] === sParam) {
+                        return sParameterName[1] === undefined ? true : sParameterName[1];
+                    }
+                }
+            };
+            console.log(getUrlParameter('id'));
+        </script>
     </body>
 </html>
