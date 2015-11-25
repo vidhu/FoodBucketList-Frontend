5d10477f7f817e5f5b30fff889c9648b82379689
vivisthebest <watermelon.star@gmail.com>
2015-11-24 20:56:08 -0500

    Create restaurant.php

---
+++ restaurant.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset = "utf-8">
+        <title>Food Bucketlist</title>
+
+        <!-- External Bootstrap 3.3.2 Framework to help with styling -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" />
+        <link rel="stylesheet" href="css/custom.css" />
+    </head>
+    <body>
+        <?php include_once('header.php'); ?>
+    </body>
+</html>
