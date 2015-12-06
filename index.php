9bbdaa0f29f2e977f1d339473162944d708eee67
alexcasella <axc@bu.edu>
2015-12-06 03:13:11 -0500

    fixed list.php and added remove glyphicons

--- index.php
+++ index.php
@@ -20,6 +20,47 @@
             </div>
         </div>
 
+        <div class="check-container">
+            <h1 style="font-family: Geneva">Your Food Bucketlist!</h1>
+            <h4 style="font-family: Geneva"><em>Check off the restaurant(s) that you have visited</em></h3>
+            
+            <div class="checkbox">
+                <label>
+                  <input type="checkbox"> One! </input>
+                  <button class="glyphicon glyphicon-remove-circle" style = "color: red; font-size: 18px;"></button>
+                </label>
+              </div>
+              
+            <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Two! </input>
+                  <button class="glyphicon glyphicon-remove-circle" style = "color: red; font-size: 18px;"></button>
+                </label>
+            </div>
+              
+            <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Three! </input>
+                  <button class="glyphicon glyphicon-remove-circle" style = "color: red; font-size: 18px;"></button>
+                </label>
+            </div>
+      
+            <div align="center">
+                <input type="button" name="button" value="Check Off" id="button" onclick="javascript:removeItem();"/>
+            </div>
+        </div>
+        
+        <script>
+        function removeItem() {
+            var x = document.getElementsByTagName("input");
+                for(i = 0; i < x.length; i++) {
+                    if (x[i].checked) {
+                        x[i].parentNode.removeChild(x[i]);
+                    }
+                }
+        }
+        </script>
+
         <script type="text/javascript">
             //Page specific functions
             function onLogin(e) {
