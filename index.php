a69b706a5f502e3e045eca8a452d891f60344ebb
veev <vivyee@bu.edu>
2015-12-06 18:39:05 -0500

    list

--- index.php
+++ index.php
@@ -46,59 +46,44 @@
 
             document.body.addEventListener("onFBLogin", onLogin, false);
             document.body.addEventListener("onFBLogout", onLogout, false);
+            
 
-            window.fbAsyncInit = function() {
-                FB.init({
-                    appId      : '1206232049392333', // App ID
-                    status     : true, // check login status
-                    cookie     : true, // enable cookies to allow the server to access the session
-                    xfbml      : true  // parse XFBML
-                });
+            console.log("in here");
+            accessToken = FB.getAuthResponse()['accessToken'];
+            var nom = new Nom(accessToken);
 
-                console.log("in here");
-                accessToken = FB.getAuthResponse()['accessToken'];
-                var nom = new Nom(accessToken);
+            var bucket_id;
+            nom.Bucket.getBuckets(function(a){
+                console.log('getting buckets');
+                console.log(a);
+                if (a.success) {
+                    console.log("in here");
+                    bucket_id = a.result[0].id;
+                } else {
+                    console.log("was not success");
+                    nom.Bucket.addBucket(user.name, "hungry", function(b) {
+                        bucket_id = b.result;
+                    });
+                }
 
-                var bucket_id;
-                nom.Bucket.getBuckets(function(a){
-                    console.log('getting buckets');
-                    console.log(a);
-                    if (a.success) {
-                        console.log("in here");
-                        bucket_id = a.result[0].id;
-                    } else {
-                        console.log("was not success");
-                        nom.Bucket.addBucket(user.name, "hungry", function(b) {
-                            bucket_id = b.result;
-                        });
-                    }
+                nom.Bucket.getItems(bucket_id, function(a) {
+                    console.log("getting items")
+                    a.result.forEach(function(restaurant) {
+                        console.log(restaurant);
 
-                    nom.Bucket.getItems(bucket_id, function(a) {
-                        console.log("getting items")
-                        a.result.forEach(function(restaurant) {
-                            console.log(restaurant);
-
-                            $.ajax({
-                                url: "http://api.fbl.vidhucraft.com/search/id/" + restaurant,
-                                dataType: "jsonp",
-                                success: function (response) {
-                                    $('#restaurants').append("<div class='item'><li><input id='" + restaurant + "' type='checkbox' value='" + response.name + "'> ");
-                                    $('#restaurants').append("<label for='" + restaurant + "'>" + response.name + "</label> </li></div>");
-                                }
-                            });
+                        $.ajax({
+                            url: "http://api.fbl.vidhucraft.com/search/id/" + restaurant,
+                            dataType: "jsonp",
+                            success: function (response) {
+                                $('#restaurants').append("<div class='item'><li><input id='" + restaurant + "' type='checkbox' value='" + response.name + "'> ");
+                                $('#restaurants').append("<label for='" + restaurant + "'>" + response.name + "</label> </li></div>");
+                            }
                         });
                     });
                 });
-            };
+            });
 
-            // Load the SDK Asynchronously
-            (function(d){
-                var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-                js = d.createElement('script'); js.id = id; js.async = true;
-                js.src = "//connect.facebook.net/en_US/all.js";
-                d.getElementsByTagName('head')[0].appendChild(js);
-            }(document));
-            
+
         </script>
     </body>
 </html>
