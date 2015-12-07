c43aab2266ac62afc729444e44fe3271c87a1979
vidhu <vidhu@bu.edu>
2015-12-07 03:15:40 -0500

    In the process

--- js/nomapi.js
+++ js/nomapi.js
@@ -30,67 +30,74 @@ var Nom = function(accessToken){
     };
     
     //Bucket API
-    this.Bucket = {
-        getBuckets: function(callback){
-            var resource = "/bucket";
-            var method = "GET";
-            var data = {
-                accessToken: accessToken
-            };
-            makeRequest(resource, method, data, callback);
-        },
-        addBucket: function(name, description, callback){
-            var resource = "/bucket";
-            var method = "POST";
-            var data = {
-                accessToken: accessToken,
-                bucketname: name,
-                bucketdescription: description
-            };
-            makeRequest(resource, method, data, callback);
-        },
-        editBucket: function(id, name, description, callback){
-            var resource = "/bucket/"+id;
-            var method = "PUT";
-            var data = {
-                accessToken: accessToken,
-                bucketname: name,
-                bucketdescription: description
-            };
-            makeRequest(resource, method, data, callback);
-        },
-        deleteBucket: function(id, callback){
-            var resource = "/bucket/"+id+"?accessToken="+accessToken;
-            var method = "DELETE";
-            var data = {
-                accessToken: accessToken
-            };
-            makeRequest(resource, method, data, callback);
-        },
-        getItems: function(id, callback){
-            var resource = "/bucket/"+id;
-            var method = "GET";
-            var data = {
-                accessToken: accessToken
-            };
-            makeRequest(resource, method, data, callback);
-        },
-        addItem: function(id, businessID, callback){
-            var resource = "/bucket/"+id+"/"+businessID;
-            var method = "POST";
-            var data = {
-                accessToken: accessToken
-            };
-            makeRequest(resource, method, data, callback);
-        },
-        deleteItem: function(id, businessID, callback){
-            var resource = "/bucket/"+id+"/"+businessID+"?accessToken="+accessToken;
-            var method = "DELETE";
-            var data = {
-                accessToken: accessToken
-            };
-            makeRequest(resource, method, data, callback);
-        }  
+    function bucket(){};
+    bucket.prototype.getBuckets = function(callback){
+        var resource = "/bucket";
+        var method = "GET";
+        var data = {
+            accessToken: accessToken
+        };
+        makeRequest(resource, method, data, function(r){
+            //Create new bucket for user
+            if(r.result.length === 0){
+                console.log("Creating a new bucket");
+                console.log(this);
+            }
+            callback(r);
+        });
     };
+    bucket.prototype.addBucket = function(name, description, callback){
+        var resource = "/bucket";
+        var method = "POST";
+        var data = {
+            accessToken: accessToken,
+            bucketname: name,
+            bucketdescription: description
+        };
+        makeRequest(resource, method, data, callback);
+    };
+    bucket.prototype.editBucket = function(id, name, description, callback){
+        var resource = "/bucket/"+id;
+        var method = "PUT";
+        var data = {
+            accessToken: accessToken,
+            bucketname: name,
+            bucketdescription: description
+        };
+        makeRequest(resource, method, data, callback);
+    };
+    bucket.prototype.deleteBucket = function(id, callback){
+        var resource = "/bucket/"+id+"?accessToken="+accessToken;
+        var method = "DELETE";
+        var data = {
+            accessToken: accessToken
+        };
+        makeRequest(resource, method, data, callback);
+    };
+    bucket.prototype.getItems = function(id, callback){
+        var resource = "/bucket/"+id;
+        var method = "GET";
+        var data = {
+            accessToken: accessToken
+        };
+        makeRequest(resource, method, data, callback);
+    };
+    bucket.prototype.addItem = function(id, businessID, callback){
+        var resource = "/bucket/"+id+"/"+businessID;
+        var method = "POST";
+        var data = {
+            accessToken: accessToken
+        };
+        makeRequest(resource, method, data, callback);
+    };
+    bucket.prototype.deleteItem = function(id, businessID, callback){
+        var resource = "/bucket/"+id+"/"+businessID+"?accessToken="+accessToken;
+        var method = "DELETE";
+        var data = {
+            accessToken: accessToken
+        };
+        makeRequest(resource, method, data, callback);
+    };
+    this.Bucket = new bucket();
 };
 
